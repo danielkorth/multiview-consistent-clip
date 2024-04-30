a1e45be211379075bd58710fbc292ef39f16c2f3
Daniel <da.korth@icloud.com>
2024-04-30 08:27:08 +0000

    add train/val/test split script

--- scripts/generate_splits.py
+++ scripts/generate_splits.py
@@ -1,26 +1,46 @@
-from pathlib import Path
+from typing import Any, Dict, List, Optional, Tuple
+
 import hydra
-from hydra import initialize, compose
-
+import lightning as L
+# import rootutils
 from omegaconf import DictConfig
-import logging
-import subprocess
-from lightning import seed_everything
-
-import json
-import pandas as pd
-import numpy as np
-np.random.seed(42)
-
 from pathlib import Path
+import json
+import numpy as np
+import pandas as pd
 
-logger = logging.getLogger(__name__)
+# rootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
+# # ------------------------------------------------------------------------------------ #
+# # the setup_root above is equivalent to:
+# # - adding project root dir to PYTHONPATH
+# #       (so you don't need to force user to install project as a package)
+# #       (necessary before importing any local modules e.g. `from src import utils`)
+# # - setting up PROJECT_ROOT environment variable
+# #       (which is used as a base for paths in "configs/paths/default.yaml")
+# #       (this way all filepaths are the same no matter where you run the code)
+# # - loading environment variables from ".env" in root dir
+# #
+# # you can remove it if you:
+# # 1. either install project as a package or move entry files to project root dir
+# # 2. set `root_dir` to "." in "configs/paths/default.yaml"
+# #
+# # more info: https://github.com/ashleve/rootutils
+# # ------------------------------------------------------------------------------------ #
 
-@hydra.main(version_base= None, config_path="../configs", config_name="data_preparation")
-def generate_splits(cfg: DictConfig):
-    seed_everything(cfg.seed)
+from src.utils import (
+    RankedLogger,
+)
 
-    data_path = Path(cfg.data_dir)
+log = RankedLogger(__name__, rank_zero_only=True)
+
+
+@hydra.main(version_base="1.3", config_path="../configs", config_name="data_preparation")
+def generate_splits(cfg: DictConfig) -> Optional[float]:
+    # set seed for random number generators in pytorch, numpy and python.random
+    if cfg.get("seed"):
+        L.seed_everything(cfg.seed, workers=True)
+
+    data_path = Path(cfg.output_dir)
 
     # Load the JSON file containing object names
     with open(data_path / 'uid_to_name.json', 'r') as file:
@@ -34,9 +54,10 @@ def generate_splits(cfg: DictConfig):
 
     # Calculate the number of samples for each split
     num_total = len(all_uids)
-    num_train = int(num_total * 0.8)
-    num_val = int(num_total * 0.1)
-    num_test = num_total - num_train - num_val
+
+    assert cfg.train_split_size + cfg.val_split_size + cfg.test_split_size == 1, "train/val/test split cannot be larger than one"
+    num_train = int(num_total * cfg.train_split_size)
+    num_val = int(num_total * cfg.val_split_size)
 
     # Split the UIDs into train, validation, and test sets
     train_uids = all_uids[:num_train]
@@ -57,7 +78,7 @@ def generate_splits(cfg: DictConfig):
     train_overfit_df = pd.DataFrame(train_uids[:1], columns=['uid'])
 
     # Save the DataFrame to a CSV file
-    train_overfit_df.to_csv(data_path / '/train_overfit.csv', index=False)
+    train_overfit_df.to_csv(data_path / 'train_overfit.csv', index=False)
 
     # Create a DataFrame for a small batch training with 8 IDs
     train_batch_df = pd.DataFrame(train_uids[:8], columns=['uid'])
@@ -65,7 +86,7 @@ def generate_splits(cfg: DictConfig):
     # Save the DataFrame to a CSV file
     train_batch_df.to_csv(data_path / 'train_batch.csv', index=False)
 
-    print("CSV files for overfitting and small batch training have been saved.")
+    log.info("CSV files have been saved.")
 
 if __name__ == "__main__":
     generate_splits()
