4e6748e1b0ef641defa3c79e1d17a708537180e5
Daniel Korth <da.korth@icloud.com>
2024-05-28 15:36:21 +0900

    update stuff

--- src/utils/visualize.py
+++ src/utils/visualize.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 
 def save_matrix_png(sim, path, type: str = 'mean'):
     plt.figure(figsize=(10, 10))
-    plt.imshow(sim, cmap='hot', interpolation='nearest')
+    plt.imshow(sim, cmap='jet', interpolation='nearest')
     if type == 'mean':
         plt.title('Mean Cosine Similarity')
-        plt.clim(0.5, 1)
+        plt.clim(0, 1)
     else: 
         plt.title('Std Cosine Similarity')
-        plt.clim(0, 0.4) 
+        plt.clim(0, 0.5) 
     plt.colorbar()
     plt.savefig(path)
